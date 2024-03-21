15c45c9ae1f653eb28957daa429c9d670051421c
Матвей Корнейчик <matveisteam7477446@gmail.com>
2024-03-21 20:53:18 +0300

    yes

--- main.cpp
+++ main.cpp
@@ -17,16 +17,8 @@ int main()
     }
 
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window(sf::VideoMode(500, 340), "Authentication",  sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(500, 400), "", sf::Style::None);
 
-    sf::Texture backgroundTexture;
-    if (!backgroundTexture.loadFromFile("background.jpg")) 
-    {
-        // Обработка ошибок загрузки изображения
-        return 1;
-    }
-    // Создание спрайта для фона
-    sf::Sprite backgroundSprite(backgroundTexture);
     
     // Загрузка изображения для иконки окна
     sf::Image icon;
@@ -39,27 +31,46 @@ int main()
     // Создаем прямоугольник размером 70х100
     sf::RectangleShape rectangle1(Vector2f(430.f, 70.f));
     sf::RectangleShape rectangle2(Vector2f(430.f, 70.f));
-    sf::RectangleShape rectangle3(Vector2f(190.f, 55.f));
+    sf::RectangleShape rectangle3(Vector2f(430.f, 70.f));
+    sf::RectangleShape square(Vector2f(25.f, 25.f));
+    sf::RectangleShape line1(Vector2f(1.f, 70.f));
+    sf::RectangleShape line2(Vector2f(1.f, 70.f));
 
     rectangle1.move(35, 80);
-    rectangle2.move(35, 170);
-    rectangle3.move(275, 260);
+    rectangle2.move(35, 160);
+    rectangle3.move(35, 255);
+    square.move(45, 340);
+    line1.move(98, 80);
+    line2.move(98, 160);
     // Устанавливаем ему цвет
-    rectangle1.setFillColor(Color(254, 128, 181));
-    rectangle2.setFillColor(Color(254, 128, 181));
-    rectangle3.setFillColor(Color(10, 161, 255));
+    rectangle1.setFillColor(Color::Black);
+    rectangle2.setFillColor(Color::Black);
+    rectangle3.setFillColor(Color(255, 193, 7));
+    square.setFillColor(Color(59, 60, 54));
+    line1.setFillColor(Color(59, 60, 54));
+    line2.setFillColor(Color(59, 60, 54));
 
-    sf::Text text(L"АВТОРИЗАЦИЯ", font);
-    text.setCharacterSize(52); // Установка размера шрифта
-    text.setFillColor(sf::Color::Black); // Установка цвета текста
+    rectangle1.setOutlineThickness(1.f); // толщина контура
+    rectangle2.setOutlineThickness(1.f); // толщина контура
+    rectangle1.setOutlineColor(sf::Color(59, 60, 54)); // цвет контура
+    rectangle2.setOutlineColor(sf::Color(59, 60, 54)); // цвет контура
+
+    sf::Text text(L"Авторизация", font);
+    text.setCharacterSize(46); // Установка размера шрифта
+    text.setFillColor(sf::Color::White); // Установка цвета текста
     // Установка начального положения текста
-    text.setPosition(140.f, 8.f); // Новые координаты (x, y)
+    text.setPosition(88.f, 8.f); // Новые координаты (x, y)
 
 
-    sf::Text entrance(L"ВОЙТИ", font);
-    entrance.setCharacterSize(36); // Установка размера шрифта
-    entrance.setFillColor(sf::Color::Black); // Установка цвета текста
-    entrance.setPosition(330.f, 265.f); // Новые координаты (x, y)
+    sf::Text entrance(L"Войти", font);
+    entrance.setCharacterSize(30); // Установка размера шрифта
+    entrance.setFillColor(sf::Color::White); // Установка цвета текста
+    entrance.setPosition(188.f, 272.f); // Новые координаты (x, y)
+
+    sf::Text rememberMe(L"Запомнить меня", font);
+    rememberMe.setCharacterSize(14); // Установка размера шрифта
+    rememberMe.setFillColor(sf::Color::White); // Установка цвета текста
+    rememberMe.setPosition(81.f, 344.f); // Новые координаты (x, y)
 
 
 
@@ -86,6 +97,11 @@ int main()
         // Обработка ошибки загрузки изображения
         return EXIT_FAILURE;
     }
+    sf::Texture galka;
+    if (!galka.loadFromFile("galka.png")) {
+        // Обработка ошибки загрузки изображения
+        return EXIT_FAILURE;
+    }
 
 
     // Создание спрайта и установка текстуры
@@ -93,38 +109,45 @@ int main()
     sf::Sprite sprite2(texture2);
     sf::Sprite sprite3(texture3);
     sf::Sprite sprite4(texture4);
+    sf::Sprite sprite_galka(galka);
 
-    sprite1.setScale(1.f / 9.f, 1.f / 9.f);
-    sprite2.setScale(1.f / 12.f, 1.f / 12.f);
+    sprite1.setScale(1.f / 10.f, 1.f / 10.f);
+    sprite2.setScale(1.f / 10.f, 1.f / 10.f);
     sprite3.setScale(1.f / 12.f, 1.f / 12.f);
     sprite4.setScale(1.f / 12.f, 1.f / 12.f);
+    sprite_galka.setScale(1.f / 19.f, 1.f / 19.f);
 
     // Установка начального положения спрайта
-    sprite1.setPosition(40.f, 87.f);
-    sprite2.setPosition(40.f, 177.f);
-    sprite3.setPosition(417.f, 185.f);
-    sprite4.setPosition(417.f, 185.f);
-
+    sprite1.setPosition(42.f, 92.f);
+    sprite2.setPosition(43.f, 172.f);
+    sprite3.setPosition(416.f, 175.f);
+    sprite4.setPosition(416.f, 175.f);
+    sprite_galka.setPosition(44.f, 339.f);
+ 
     sf::Text login("", font);
     login.setCharacterSize(38); // Установка размера шрифта
-    login.setFillColor(sf::Color::Black); // Установка цвета текста
+    login.setFillColor(sf::Color::White); // Установка цвета текста
     login.setPosition(110.f, 93.f); // Новые координаты (x, y)
 
     sf::Text password1("", font);
     password1.setCharacterSize(38); // Установка размера шрифта
-    password1.setFillColor(sf::Color::Black); // Установка цвета текста
-    password1.setPosition(110.f, 183.f); // Новые координаты (x, y)
+    password1.setFillColor(sf::Color::White); // Установка цвета текста
+    password1.setPosition(110.f, 173.f); // Новые координаты (x, y)
 
     sf::Text password2("", font);
     password2.setCharacterSize(72); // Установка размера шрифта
-    password2.setFillColor(sf::Color::Black); // Установка цвета текста
-    password2.setPosition(110.f, 183.f); // Новые координаты (x, y)
+    password2.setFillColor(sf::Color::White); // Установка цвета текста
+    password2.setPosition(110.f, 173.f); // Новые координаты (x, y)
 
     // првоерка, какое поле выбпал для ввода
     bool isRectangle1Clicked = false;
     bool isRectangle2Clicked = false;
     bool isPasswordOpen = false;
     bool isTextPasswordOpen = false;
+    bool isRememberMeOn = false;
+    // для перемещения окна
+    bool isDragging = false;
+    sf::Vector2i clickPosition;
 
     while (window.isOpen())
     {
@@ -133,7 +156,21 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window.close();
 
+            else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+            {
+                // Запоминаем позицию, в которой была нажата кнопка мыши
+                isDragging = true;
+                clickPosition = sf::Mouse::getPosition(window);
+            }
+            else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
+            {
+                // Когда кнопка мыши отпущена, заканчиваем перетаскивание
+                isDragging = false;
+            }
+            // проверка на поле для ввода
             if (event.type == sf::Event::MouseButtonPressed)
             {
                 if (event.mouseButton.button == sf::Mouse::Left)
@@ -150,8 +187,24 @@ int main()
                         isRectangle2Clicked = true;
                     }
                 }
-
             }
+            // проверка на галочку
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                    if (square.getGlobalBounds().contains(mousePos.x, mousePos.y) and !isRememberMeOn)
+                    {
+                        isRememberMeOn = true;
+                    }
+                    else if (square.getGlobalBounds().contains(mousePos.x, mousePos.y) and isRememberMeOn)
+                    {
+                        isRememberMeOn = false;
+                    }
+                }
+            }
+
             if (event.type == sf::Event::TextEntered)
             {
                 if (event.text.unicode < 128)
@@ -203,6 +256,16 @@ int main()
 
         }
 
+        if (isDragging)
+        {
+            // Вычисляем разницу между текущей позицией мыши и позицией, где была нажата кнопка мыши
+            sf::Vector2i delta = sf::Mouse::getPosition(window) - clickPosition;
+            // Получаем текущее положение окна и сдвигаем его на delta
+            window.setPosition(sf::Vector2i(window.getPosition()) + delta);
+            // Обновляем позицию clickPosition
+            clickPosition = sf::Mouse::getPosition(window);
+        }
+
         /*if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
             // Изменение размера окна на 1920x1080
             window.setSize(sf::Vector2u(1920, 1080));
@@ -210,20 +273,24 @@ int main()
             window.setPosition(sf::Vector2i((desktop.width - window.getSize().x) / 2, (desktop.height - window.getSize().y) / 2));
         }  */
 
-        window.clear();
-        window.draw(backgroundSprite);
+        window.clear(sf::Color(10, 10, 10));
         window.draw(rectangle1);
         window.draw(rectangle2);
         window.draw(rectangle3);
+        window.draw(square);
+        window.draw(line1);
+        window.draw(line2);
         window.draw(text);
         window.draw(entrance);
         window.draw(login);
+        window.draw(rememberMe);
         if (isTextPasswordOpen) window.draw(password1);
         else window.draw(password2);
         window.draw(sprite1);
         window.draw(sprite2);
         if (isPasswordOpen)  window.draw(sprite4);
         else  window.draw(sprite3);
+        if (isRememberMeOn) window.draw(sprite_galka);
         window.display();
     }
 
